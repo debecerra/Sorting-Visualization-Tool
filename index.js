9befde7eac7a25a4b6b9e867e80ceb7d11534dec
Diego Becerra <Diego.E.Becerra@outlook.com>
2020-10-18 22:17:11 -0600

    Improve visuals and list initialization

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 //jshint esversion:6
 
-const DEFAULT_SIZE = 10;
+/********
+ Globals
+ ********/
+const DEFAULT_SIZE = 15;
 
+list = null;
+
+/*
+  ListElem:
+*/
 function ListElem(value) {
+  // Throw an error if this.maxValue is not defined
+  if (this.maxValue == null) {
+    throw "Error: max value not set before creating ListElem object";
+  }
+
+  // Set value
   this.value = value;
+
+  // Set width and height of HTML component
+  this.width = (this.maxValue <= 16 ? 40 : 15);
+  this.height = 0.8 * $(".display-area").height() * (this.value / this.maxValue);
+  console.log(this.height);
+
+  // Create HTML component
   this.elem = $("<div>")
     .addClass("array-element")
-    .css("height", 20 * this.value)
-    .css("width", 20);
+    .css("height", this.height)
+    .css("width", this.width);
 
 
   this.draw = function() {
-    console.log($("display-area"));
     $(".display-area").append(this.elem);
   };
 }
 
+/*
+ List:
+ */
 function List(length) {
   this.length = length;
   this.elements = [];
@@ -35,6 +58,8 @@ function List(length) {
       listElem.draw();
     });
 
+    var currentSize = $(".display-area").css("height");
+    $(".display-area").css("min-height", currentSize);
 
   };
 
@@ -46,8 +71,16 @@ function List(length) {
       this.elements[j] = temp;
     }
   };
-
-  this.init();
 }
 
-let list = new List(DEFAULT_SIZE);
+/*
+  refreshActiveList
+ */
+
+function refreshActiveList(size) {
+  list = new List(size);
+  ListElem.prototype.maxValue = size;
+  list.init();
+}
+
+refreshActiveList(DEFAULT_SIZE);
