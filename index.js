d43decbd5811bc84d29faa1af04639e44715ab23
Diego Becerra <Diego.E.Becerra@outlook.com>
2020-10-20 23:28:08 -0600

    Start swap function

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const DEFAULT_SIZE = 15;
 
 list = null;
 
-/*
-  ListElem:
-*/
+// Define the ListElem object
 function ListElem(value) {
   // Throw an error if this.maxValue is not defined
   if (this.maxValue == null) {
@@ -20,30 +18,40 @@ function ListElem(value) {
   this.value = value;
 
   // Set width and height of HTML component
+  this.maxHeight = 0.8 * $(".display-area").height();
   this.width = (this.maxValue <= 16 ? 40 : 15);
-  this.height = 0.8 * $(".display-area").height() * (this.value / this.maxValue);
-  console.log(this.height);
+  this.height = this.maxHeight * (this.value / this.maxValue);
 
-  // Create HTML component
+  // Create UI component
   this.elem = $("<div>")
     .addClass("array-element")
     .css("height", this.height)
     .css("width", this.width);
 
-
-  this.draw = function() {
+  // Initialize the UI component
+  this.initUI = function() {
     $(".display-area").append(this.elem);
+  };
+
+  // Update the state of the ListElem
+  this.update = function(newValue) {
+    // Update the object properties
+    this.value = newValue;
+    this.height = this.maxHeight * (this.value / this.maxValue);
+
+    // Update the UI component
+    this.elem.css("height", this.height);
+    this.elem.css("background-color", "blue");
   };
 }
 
-/*
- List:
- */
+// Define the List object
 function List(length) {
   this.length = length;
-  this.elements = [];
+  this.elements = [];  // An array of ListElem
 
-  // Initialize the List with random values
+
+  // Initialize the List with randomly shuffled integer values
   this.init = function() {
     // Create the list of numbers
     for (var i = 1; i <= length; i++) {
@@ -55,7 +63,7 @@ function List(length) {
 
     // Draw the list to the display
     this.elements.forEach(function(listElem) {
-      listElem.draw();
+      listElem.initUI();
     });
 
     var currentSize = $(".display-area").css("height");
@@ -63,6 +71,7 @@ function List(length) {
 
   };
 
+  // Shuffle the elements of the List
   this.shuffle = function() {
     for (let i = this.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * i);
@@ -86,16 +95,38 @@ function refreshActiveList(size) {
 
 refreshActiveList(DEFAULT_SIZE);
 
-// Event Handlers
+// Handle click on Sort button
+// TODO
+
+// Handle click on Reset button
 $("#reset-btn").on("click", function(){
   size = list.length;
   refreshActiveList(size);
 });
 
+// Handle input into number of elements field
 $("#elem-num-input").on("input", function(){
-  console.log($("#elem-num-input").val());
+  // Get the value of the input tag
   num = $("#elem-num-input").val();
+
+  // Assert 5 <= num <= 100
   num = num < 5 ? 5 : num;
   num = num > 100 ? 100 : num;
+
+  // Create a new active list with specified size
   refreshActiveList(num);
 });
+
+/********************
+ * Sorting Algorithms
+ ********************/
+
+// Swap two positions of List
+function swap(i, j){
+  if (!Number.isInteger(i) || !Number.isInteger(j)){
+    throw "Error: i and j must be integers";
+  }
+
+  // Swap the values of the ListElems
+
+}
